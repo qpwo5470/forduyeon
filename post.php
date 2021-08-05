729aa4371cd1f542ad9e984f0f5303f8ffe39f7f
qpwo5470 <qpwo5470@gmail.com>
2021-08-05 23:32:42 +0900

    added post.php

--- post.php
+++ post.php
@@ -2,19 +2,18 @@
 $json_data = $_POST['json'];
 $data = json_decode($json_data);
 
-$conn = mysqli_connect(
-    'localhost',
-    'duyeon',
-    'endus',
-    'test');
-
-$id = $data["id"];
-$name = $data["name"];
-$state = $data["state"];
-
-$sql = "INSERT INTO device_state(id, name, state) VALUES ('$id', '$name', '$state')";
-$result = mysqli_query($conn, $sql);
-echo $result;
-
-mysqli_close($conn);
+//$conn = mysqli_connect(
+//    'localhost',
+//    'duyeon',
+//    'endus',
+//    'test');
+//
+//$id = $data["id"];
+//$name = $data["name"];
+//$state = $data["state"];
+//
+//$sql = "INSERT INTO device_state(id, name, state) VALUES ('$id', '$name', '$state')";
+//mysqli_query($conn, $sql);
+//
+//mysqli_close($conn);
 ?>
